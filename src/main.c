401ffee3b48905a786796a17e666e036a57af537
Antonio Delgado Bejarano <antdelga@c3r10s4.42malaga.com>
2023-05-24 17:45:02 +0200

    [2023-05-24 17:45:02] - Little changes by antdelga

--- src/main.c
+++ src/main.c
@@ -55,16 +55,32 @@ void	ft_leaks(void)
 	system("leaks -q minishell");
 }
 
+void sigint_handler(int sig) {
+	(void) sig;
+	rl_on_new_line();
+	rl_redisplay();
+	ft_putstr_fd("  ", 1);
+	ft_printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*input;
 	t_data	data;
+
 
 	atexit(ft_leaks);
 	ft_printf("%s", &(HEADER));
 	(void)argc;
 	(void)argv;
 	init_data(&data, env);
+	
+	// Establecer el controlador de señales
+    signal(SIGINT, sigint_handler);
+	
 	while (1)
 	{
 		input = readlineplus(&data);
