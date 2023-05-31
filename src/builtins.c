5d68a88c90991e274d9a7035902bcf6230131011
javiersa <javiersa@student.42malaga.com>
2023-05-31 21:59:55 +0200

    [2023-05-31 21:59:55] - Little changes by javiersa

--- src/builtins.c
+++ src/builtins.c
@@ -1,9 +1,11 @@
 #include "minishell.h"
 
-void bt_cd(t_data *data, t_command *cmd)
+//Te he quitado el leak que habia reasignando envs (pero yo haria la reasignacion d los envs (OLDPWD y PWD) cuando este la funcion export llamando a esa)
+void	bt_cd(t_data *data, t_command *cmd)
 {
 	int		i;
 	char	*aux;
+	char	aux2[2048];
 
 	if (!cmd->opt[1])
 	{
@@ -13,19 +15,17 @@ void bt_cd(t_data *data, t_command *cmd)
 		free(aux);
 	}
 	else
-	{
-		// printf("OPT: %s\n", cmd->opt[1]);
 		if (chdir(cmd->opt[1]) != 0)
 			return (perror("cd"));
-	}
 	i = 0;
-	while(ft_strncmp(data->env[i], "PWD=", 4) != 0)
+	while (ft_strncmp(data->env[i], "PWD=", 4) != 0)
 		i++;
 	if (data->env[i])
 	{
-		if (getcwd(data->env[i], 2048) == NULL)
-        	return (perror("pwd"));
-		data->env[i] = ft_strjoin("PWD=", data->env[i]);
+		ft_free_and_null((void **)&data->env[i]);
+		if (getcwd(aux2, 2048) == NULL)
+			return (perror("pwd"));
+		data->env[i] = ft_strjoin("PWD=", aux2);
 	}
 }
 
@@ -35,4 +35,3 @@ int	select_builtin(t_data *data, t_command *comando)
 		return (bt_cd(data, comando), 1);
 	return (0);
 }
-
