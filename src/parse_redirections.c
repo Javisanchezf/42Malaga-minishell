2f85214ec5b19e330c17887d0f05d2bbeab6b67c
Javier Sánchez Ferández <javiersanchezf@hotmail.com>
2023-06-01 11:47:03 +0200

    [2023-06-01 11:47:03] - Little changes by javiersa

--- src/parse_redirections.c
+++ src/parse_redirections.c
@@ -1,11 +1,55 @@
 #include "minishell.h"
 
-// int archivo = open(nombreArchivo, O_WRONLY | O_CREAT, 0644);
+void	input_parse(t_data *data, int i, int j, int cont)
+{
+	int		k;
+	int		file;
 
+	k = 0;
+	while (data->cmd[i].opt[j][k] && (data->cmd[i].opt[j][k] == '<' || ft_isspace(data->cmd[i].opt[j][k])))
+		k++;
+	data->cmd[i].input_type = cont;
+	ft_free_and_null((void **)&data->cmd[i].output);
+	data->cmd[i].output = ft_substr(data->cmd[i].opt[j], k, ft_strlen(data->cmd[i].opt[j]) - k);
+	if (cont == 1)
+	{
+		file = open(data->cmd[i].output, O_RDONLY, 0644);
+		close(file);
+		//if (file == -1 && errno == EACCES) sustituir todo por: minishell: a.txt: Permission denied
+	}
+	//el << rarito
+	//liberar esa parte del split
+}
+
+void	output_parse(t_data *data, int i, int j, int cont)
+{
+	int		k;
+	int		file;
+
+	k = 0;
+	while (data->cmd[i].opt[j][k] && (data->cmd[i].opt[j][k] == '>' || ft_isspace(data->cmd[i].opt[j][k])))
+		k++;
+	data->cmd[i].output_type = cont;
+	ft_free_and_null((void **)&data->cmd[i].output);
+	data->cmd[i].output = ft_substr(data->cmd[i].opt[j], k, ft_strlen(data->cmd[i].opt[j]) - k);
+	if (cont == 1)
+		file = open(data->cmd[i].output, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	else
+		file = open(data->cmd[i].output, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	//if (file == -1 && errno == EACCES) sustituir todo por: minishell: a.txt: Permission denied
+	close(file);
+	//liberar esa parte del split
+}
 int	nose(t_data *data, int i, int j, char type)
 {
 	int	cont;
 
+	cont = 0;
+	while (data->cmd[i].opt[j][cont] && (data->cmd[i].opt[j][cont] == '<' || data->cmd[i].opt[j][cont] == '>' || ft_isspace(data->cmd[i].opt[j][cont])))
+		cont++;
+	if (!data->cmd[i].opt[j][cont])
+		return (ft_putstr_fd("\033[31;1mminishell: syntax error near\
+ unexpected token\n\033[0m", 2), clean_commands(data), 1);
 	cont = 0;
 	while (data->cmd[i].opt[j][cont] == '<' || data->cmd[i].opt[j][cont] == '>')
 	{
@@ -17,6 +61,10 @@ int	nose(t_data *data, int i, int j, char type)
 	if (cont > 2)
 		return (ft_putstr_fd("\033[31;1mminishell: syntax error near\
  unexpected token\n\033[0m", 2), clean_commands(data), 1);
+	if (type == '>')
+		output_parse(data, i, j, cont);
+	else if (type == '<')
+		input_parse(data, i, j, cont);
 	return (0);
 }
 
@@ -26,6 +74,8 @@ int	parse_redirections(t_data *data, int i)
 
 	data->cmd[i].input = ft_strdup("");
 	data->cmd[i].output = ft_strdup("");
+	data->cmd[i].input_type = 0;
+	data->cmd[i].output_type = 0;
 	j = -1;
 	while (data->cmd[i].opt[++j])
 	{
